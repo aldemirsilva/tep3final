ff58ed196d7fcbdc4fc352d6327c200f7d2f2fe3
Aldemir Silva <aldemir.silva@icomp.ufam.edu.br>
2024-11-27 19:42:38 -0400

    Adding ex12.sh

---
+++ ex12.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for((i=60000; i<=63000; i++));do
+  for((j=2; j<=i/2; j++)); do
+    if [ $((i%j)) -eq 0 ]; then
+      continue 2
+    fi
+  done
+  primos+=("$i")
+done
+for((l=0; l<${#primos[@]}; l++)); do
+  printf "%-8d" ${primos[l]}
+  if [ $(((l+1)%10)) -eq 0 ]; then
+    printf "\n"
+  fi
+done
+printf "\n"
