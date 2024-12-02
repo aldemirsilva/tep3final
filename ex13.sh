d0ee698fd271fd10bcf5c19ab5913184e684e593
Aldemir Silva <aldemir.silva@icomp.ufam.edu.br>
2024-12-02 19:28:56 -0400

    Adding ex13.sh

---
+++ ex13.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+numeros=()
+while((${#numeros[@]} < 5)); do
+  ((n=RANDOM%50+1))
+  if [[ ${numeros[@]} =~ $n ]]
+    then
+      continue
+    else
+      numeros+=("$n")
+  fi
+done
+
+if [[ "$1" -eq 0 ]]; then
+  for((l=0; l<${#numeros[@]}; l++)); do
+    printf "%d " ${numeros[l]}
+  done
+  printf "\n"
+  else if [[ "$1" -eq 1 ]]; then
+    `echo $(date) > numeros.txt`
+    `echo ${numeros[@]} >> numeros.txt`
+  fi
+fi
